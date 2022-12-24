9df71677d30bb69590d30006f5ad0eb6ad62c41e
DavidBonvin <ledave.musique@gmail.com>
2022-12-24 02:44:47 +0100

    maj

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\InvoiceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\InvoiceRepository;
 
 #[ORM\Entity(repositoryClass: InvoiceRepository::class)]
+#[ApiResource]
 class Invoice
 {
     #[ORM\Id]
